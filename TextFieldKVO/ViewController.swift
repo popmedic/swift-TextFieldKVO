bddd55dc87160d66732987559d0763f7b5f62e79
kscardina <popmedic@gmail.com>
2018-07-12 08:00:27 -0600

    comments

--- TextFieldKVO/ViewController.swift
+++ TextFieldKVO/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
     private var lastNameObservingView:UIView? = nil
     @IBOutlet weak var lastNameLabel: UILabel!
     @IBOutlet weak var lastNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // add the observer
         lastNameTextField.addObserver(
             self,
             forKeyPath: "hidden",
@@ -22,23 +24,32 @@ class ViewController: UIViewController {
             context: &self.lastNameTextFieldHiddenContext
         )
     }
+    
+    /// function will be called whenever an added observer is triggered
     override func observeValue(
         forKeyPath keyPath: String?,
         of object: Any?,
         change: [NSKeyValueChangeKey : Any]?,
         context: UnsafeMutableRawPointer?
     ) {
-        if let newValue = change?[NSKeyValueChangeKey.newKey] as? Bool,
-            context == &self.lastNameTextFieldHiddenContext {
-            self.lastNameLabel.isHidden = newValue
+        // make sure it is our text field isHidden observer
+        if context == &self.lastNameTextFieldHiddenContext {
+            // get the new value that was set
+            if let newValue = change?[NSKeyValueChangeKey.newKey] as? Bool {
+                // do what needs to be done when the observer is triggered
+                self.lastNameLabel.isHidden = newValue
+            }
         }
     }
+    
     deinit {
+        // remove the observer
         if let view = lastNameObservingView {
             view.removeObserver(self, forKeyPath: "hidden")
             lastNameObservingView = nil
         }
     }
+    
     @IBAction func showHideButtonAction(_ sender: Any) {
         self.lastNameTextField.isHidden = !self.lastNameTextField.isHidden
     }
